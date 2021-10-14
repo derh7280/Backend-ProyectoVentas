b9470eabb6a35a083952a05a0dddae62d6d72310
Duhan Renteria <derh7280@hotmail.com>
2021-10-14 00:23:51 -0500

    Asunto: Backend con auth0 cuerpo: Se utiliza patron MVC pero solo se trabaja con VC, para la gestion de informacion en la base de datos, rutas de producto, usuarios y ventas, incluye autenticacion con auth0

---
+++ controllers/productos/controller.js
@@ -0,0 +1,46 @@
+import { ObjectId } from 'mongodb';
+import { getDB } from '../../db/db.js';
+
+const queryAllProducts = async (callback) => {
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('productos').find({}).limit(50).toArray(callback);
+};
+
+const crearProducto = async (datosProducto, callback) => {
+  if (
+    Object.keys(datosProducto).includes('nombre') &&
+    Object.keys(datosProducto).includes('precio') &&
+    Object.keys(datosProducto).includes('estado')
+  ) {
+    const baseDeDatos = getDB();
+    // implementar código para crear vehículo en la BD
+
+    await baseDeDatos.collection('productos').insertOne(datosProducto, callback);
+  } else {
+    return 'error';
+  }
+};
+
+const consultarProducto = async (id, callback) => {
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('productos').findOne({ _id: new ObjectId(id) }, callback);
+};
+
+const editarProducto = async (id, edicion, callback) => {
+  const filtroProducto = { _id: new ObjectId(id) };
+  const operacion = {
+    $set: edicion,
+  };
+  const baseDeDatos = getDB();
+  await baseDeDatos
+    .collection('productos')
+    .findOneAndUpdate(filtroProducto, operacion, { upsert: true, returnOriginal: true }, callback);
+};
+
+const eliminarProducto = async (id, callback) => {
+  const filtroProducto = { _id: new ObjectId(id) };
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('productos').deleteOne(filtroProducto, callback);
+};
+
+export { queryAllProducts, crearProducto, consultarProducto, editarProducto, eliminarProducto };
---
+++ server.js
@@ -0,0 +1,47 @@
+// hacer el import de express tradicional
+// const express = require('express');
+
+// hacer el nuevo import
+import Express from 'express';
+import Cors from 'cors';
+import dotenv from 'dotenv';
+import { conectarBD } from './db/db.js';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
+
+import rutasProducto from './views/productos/rutas.js';
+import rutasUsuario from './views/usuarios/rutas.js';
+import rutasVenta from './views/ventas/rutas.js';
+
+dotenv.config({ path: './.env' });
+
+const app = Express();
+
+app.use(Express.json());
+app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://dev-6w94aqz7.us.auth0.com/.well-known/jwks.json',
+  }),
+  audience: 'http://api-autenticacion-proyecto-ventas-mintic.com',
+  issuer: 'https://dev-6w94aqz7.us.auth0.com/',
+  algorithms: ['RS256'],
+});
+
+app.use(jwtCheck);
+
+app.use(rutasProducto);
+app.use(rutasUsuario);
+app.use(rutasVenta);
+
+const main = () => {
+  return app.listen(process.env.PORT, () => {
+    console.log(`Servidor conectado en el puerto ${process.env.PORT}`);
+  });
+};
+
+conectarBD(main);
